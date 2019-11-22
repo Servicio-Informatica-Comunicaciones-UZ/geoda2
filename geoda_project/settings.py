2105be4a53241bf06ef39e4011ca5a15d5c652c3
Enrique Matías Sánchez (Quique) <quique@unizar.es>
2019-11-22 14:35:11 +0100

    Dockerizar la aplicación (2)

--- geoda_project/settings.py
+++ geoda_project/settings.py
@@ -21,12 +21,26 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "o+^%z1pkglqpsxjhdaj_u)9pfpybx0l7ko$x!d=j+y0x8qn*c)"
+SECRET_KEY = os.environ.get(
+    "DJANGO_SECRET_KEY", "o+^%z1pkglqpsxjhdaj_u)9pfpybx0l7ko$x!d=j+y0x8qn*c)"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", False) == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]  # []
+
+
+DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL", "root@localhost")
+EMAIL_BACKEND = os.environ.get(
+    "EMAIL_BACKEND", "django.core.mail.backends.console.EmailBackend"
+)
+EMAIL_HOST = os.environ.get("EMAIL_HOST", "localhost")
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_PORT = os.environ.get("EMAIL_PORT", 587)
+EMAIL_USE_LOCALTIME = True
+EMAIL_USE_TLS = True
 
 
 # Application definition
@@ -45,6 +59,7 @@ INSTALLED_APPS = [
     "crispy_forms",  # https://github.com/django-crispy-forms/django-crispy-forms
     "django_filters",  # https://pypi.org/project/django-filter/
     "django_tables2",  # https://github.com/jieter/django-tables2
+    "social_django",  # https://github.com/python-social-auth/social-app-django
 ]
 
 MIDDLEWARE = [
@@ -59,6 +74,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "geoda_project.urls"
 
+SITE_URL = os.environ.get("SITE_URL")
+
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
@@ -70,6 +87,8 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "social_django.context_processors.backends",
+                "social_django.context_processors.login_redirect",
             ]
         },
     }
@@ -83,13 +102,33 @@ WSGI_APPLICATION = "geoda_project.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "geoda2b",
-        "USER": "geoda2b",
-        "PASSWORD": "geoda2b",
-        "HOST": "localhost",  # Or an IP Address that your DB is hosted on
-        "PORT": "3306",
-    }
+        "ENGINE": os.environ.get(
+            "ENGINE", "django.db.backends.mysql"
+        ),  # Database engine
+        "NAME": os.environ.get("DB_NAME"),  # Database name
+        "USER": os.environ.get("DB_USER"),  # Database user
+        "PASSWORD": os.environ.get("DB_PASSWORD"),  # Database password
+        "HOST": os.environ.get("DB_HOST"),  # Set to empty string for localhost.
+        "PORT": "",  # Set to empty string for default.
+        # Additional database options
+        "OPTIONS": {
+            "charset": "utf8mb4"  # Requires `innodb_default_row_format = dynamic`
+        },
+    },
+    "identidades": {
+        "ENGINE": os.environ.get(
+            "ID_ENGINE", "django.db.backends.oracle"
+        ),  # Database engine
+        "NAME": os.environ.get("ID_NAME"),  # Database name
+        "USER": os.environ.get("ID_USER"),  # Database user
+        "PASSWORD": os.environ.get("ID_PASSWORD"),  # Database password
+        "HOST": os.environ.get("ID_HOST"),  # Set to empty string for localhost.
+        "PORT": os.environ.get("ID_PORT"),  # Set to empty string for default.
+        # Additional database options
+        # "OPTIONS": {
+        #     "charset": "WE8ISO8859P1",
+        # }
+    },
 }
 
 
@@ -98,7 +137,7 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"  # noqa
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"  # noqa: E501
     },
     {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
     {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
@@ -111,7 +150,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "es-es"
 
-TIME_ZONE = "Europe/Madrid"
+TIME_ZONE = os.environ.get("TZ")
 
 USE_I18N = True
 
@@ -123,12 +162,93 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
 
 AUTH_USER_MODEL = "accounts.CustomUser"
-LOGIN_REDIRECT_URL = "mis-asignaturas"
+LOGIN_URL = f"{SITE_URL}login/saml/?idp=lord"
+LOGIN_REDIRECT_URL = "mis-cursos"
 LOGOUT_REDIRECT_URL = "home"
+
+
+# ## SAML with Python Social Auth ## #
+# https://python-social-auth.readthedocs.io/en/latest/backends/saml.html
+
+AUTHENTICATION_BACKENDS = (
+    "social_core.backends.saml.SAMLAuth",
+    "django.contrib.auth.backends.ModelBackend",
+)
+# When using PostgreSQL,
+# it’s recommended to use the built-in JSONB field to store the extracted extra_data.
+# To enable it define the setting:
+# SOCIAL_AUTH_POSTGRES_JSONFIELD = True
+# Identifier of the SP entity (must be a URI)
+SOCIAL_AUTH_SAML_SP_ENTITY_ID = os.environ.get(
+    "SOCIAL_AUTH_SAML_SP_ENTITY_ID", "http://localhost:8001/accounts/metadata"
+)
+SOCIAL_AUTH_SAML_SP_PUBLIC_CERT = os.environ.get("SOCIAL_AUTH_SAML_SP_PUBLIC_CERT")
+SOCIAL_AUTH_SAML_SP_PRIVATE_KEY = os.environ.get("SOCIAL_AUTH_SAML_SP_PRIVATE_KEY")
+SOCIAL_AUTH_SAML_ORG_INFO = {
+    "en-US": {
+        "name": "geoda2",
+        "displayname": "Gestión de la Enseñanza Online",
+        "url": f"{SITE_URL}",
+    }
+}
+SOCIAL_AUTH_SAML_TECHNICAL_CONTACT = {
+    "givenName": os.environ.get("SOCIAL_AUTH_SAML_TECHNICAL_CONTACT_NAME"),
+    "emailAddress": os.environ.get("SOCIAL_AUTH_SAML_TECHNICAL_CONTACT_MAIL"),
+}
+SOCIAL_AUTH_SAML_SUPPORT_CONTACT = {
+    "givenName": os.environ.get("SOCIAL_AUTH_SAML_SUPPPORT_CONTACT_NAME"),
+    "emailAddress": os.environ.get("SOCIAL_AUTH_SAML_SUPPPORT_CONTACT_MAIL"),
+}
+# Si se cambia el backend de autenticación, actualizar clean() en InvitacionForm
+SOCIAL_AUTH_SAML_ENABLED_IDPS = {
+    "lord": {
+        "entity_id": os.environ.get("IDP") + "/saml2/idp/metadata.php",
+        "url": os.environ.get("IDP") + "/saml2/idp/SSOService.php",
+        "slo_url": os.environ.get("IDP") + "/saml2/idp/SingleLogoutService.php",
+        "x509cert": os.environ.get("X509CERT"),
+        "attr_user_permanent_id": "uid",
+        "attr_full_name": "cn",  # "urn:oid:2.5.4.3"
+        "attr_first_name": "givenName",  # "urn:oid:2.5.4.42"
+        "attr_last_name": "sn",  # "urn:oid:2.5.4.4"
+        "attr_username": "uid",  # "urn:oid:0.9.2342.19200300.100.1.1"
+        # "attr_email": "email",            # "urn:oid:0.9.2342.19200300.100.1.3"
+    }
+}
+
+SOCIAL_AUTH_PIPELINE = (
+    "social_core.pipeline.social_auth.social_details",
+    "social_core.pipeline.social_auth.social_uid",
+    "social_core.pipeline.social_auth.auth_allowed",
+    "social_core.pipeline.social_auth.social_user",
+    "social_core.pipeline.user.get_username",
+    # "social_core.pipeline.mail.mail_validation",
+    # "social_core.pipeline.social_auth.associate_by_email",
+    "social_core.pipeline.user.create_user",
+    # Actualizar con los datos de Gestión de Identidades
+    "accounts.pipeline.get_identidad",
+    "social_core.pipeline.social_auth.associate_user",
+    "social_core.pipeline.social_auth.load_extra_data",
+    "social_core.pipeline.user.user_details",
+)
+
+SOCIAL_AUTH_URL_NAMESPACE = "social"
 
 # CRISPY FORMS
 CRISPY_TEMPLATE_PACK = "bootstrap4"
+
+# DATOS DE LA PLATAFORMA
+URL_PLATAFORMA = os.environ.get("URL_PLATAFORMA")
+GEO_TOKEN = os.environ.get("GEO_TOKEN")
+GEODAWS_TOKEN = os.environ.get("GEODAWS_TOKEN")
+API_URL = f"{URL_PLATAFORMA}/webservice/rest/server.php"
+
+# DATOS DE WS GESTIÓN DE IDENTIDADES para vincular usuarios externos
+WSDL_VINCULACIONES = os.environ.get("WSDL_VINCULACIONES")
+USER_VINCULACIONES = os.environ.get("USER_VINCULACIONES")
+PASS_VINCULACIONES = os.environ.get("PASS_VINCULACIONES")
