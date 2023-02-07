613fa8e08b1ba64842df8bf12a6ee14beac62bb9
Enrique Matías Sánchez (Quique) <quique@unizar.es>
2023-02-07 09:35:50 +0100

    refactor delete_matricula_automatica()

--- geo/api.py
+++ geo/api.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from ninja import NinjaAPI
 from .models import MatriculaAutomatica
 from .schema import NotFoundSchema
@@ -5,15 +6,12 @@ from .schema import NotFoundSchema
 api = NinjaAPI()
 
 
-@api.delete('/matricula-automatica/{registro_id}', response={204: None, 404: NotFoundSchema})
+@api.delete('/matricula-automatica/{registro_id}', response={204: None})
 def delete_matricula_automatica(request, registro_id: int):
-    """Delete an existing track from the database"""
-    try:
-        registro = MatriculaAutomatica.objects.get(pk=registro_id)
-        registro.delete()
-        return 204  # No content
-    except registro.DoesNotExist as e:
-        return 404, {'message': 'No se encontró ese registro de matrícula automática.'}
+    """Borra un registro de matrícula automática"""
+    registro = get_object_or_404(MatriculaAutomatica, id=registro_id)
+    registro.delete()
+    return 204, None  # No content
 
 
 @api.patch('/matricula-automatica-toggle/{registro_id}', response={200: None, 404: NotFoundSchema})
@@ -24,5 +22,5 @@ def toggle_matricula_automatica(request, registro_id: int):
         registro.active = not registro.active
         registro.save()
         return 200  # OK
-    except registro.DoesNotExist as e:
+    except registro.DoesNotExist:  # as e:
         return 404, {'message': 'No se encontró ese registro de matrícula automática.'}
